33756204e4f7d8dfd8851a521d0b5b1bdeaa6555
daniel-williams <mail@danielwilliams.us>
2015-11-25 14:48:31 -0800

    Adding react-redux and redux store to app.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Router, {Route} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
 import App from './components/App';
 import Results from './components/Results';
 import Voting from './components/Voting';
 
+const store = createStore(reducer);
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: ['Sunshine', '28 Days Later'],
+            tally: {
+                'Sunshine': 2
+            }
+        }
+    }
+})
 const routes = (
     <Route component={App}>
         <Route path="/results" component={Results} />
@@ -13,6 +28,8 @@ const routes = (
 );
 
 ReactDom.render(
-    <Router>{routes}</Router>,
+    <Provider store={store}>
+        <Router>{routes}</Router>
+    </Provider>,
     document.getElementById('app')
 );
